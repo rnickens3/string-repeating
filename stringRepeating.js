bbf35b0ade600b4281f3169a93e252f7fd88b127
Robert <rnickens3@gmail.com>
2025-12-26 13:09:31 -0600

    Implement string repetition function
    Added a function to repeat a string a specified number of times.

---
+++ stringRepeating.js
@@ -0,0 +1,19 @@
+/*
+Completed December 26th, 2025 
+FreeCodeCamp.org Assignment: In this lab, you will create a function that repeats a given string a specific number of times.
+Concepts Used: Accumulator pattern, control flow order, early exits, concatenation
+*/
+
+function repeatStringNumTimes(string, number) {
+  let result = "";  //This is called an accumulator. It is a variable that will get added to instead of reassigned. 
+
+  if (number <= 0) {
+    return "";  
+  } 
+  for (let i = 0; i < number; i++) {
+    result += string;  //I had to remind myself how to concatenate here
+  }
+  return result;
+}
+
+console.log(repeatStringNumTimes("abc", 3)); 
